a2d5820f394c1a652b79005c8500a4293ca22d67
Himanshu Seervi <himseervi@gmail.com>
2024-12-24 00:09:02 -0500

    Working on the animations

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,29 @@
+import Logo from "../assets/logo.svg";
+import XSocial from '@/assets/social-x.svg';
+import InstaSocial from '@/assets/social-instagram.svg';
+import YTSocial from '@/assets/social-youtube.svg';
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return <footer className="py-5 border border-white/15">
+    <div className="container">
+      <div className="flex flex-col lg:flex-row gap-5 lg:gap-7">
+        <div className="flex gap-2 lg:items-center lg:flex-1">
+          <Logo className="h-6 w-6" />
+          <div className="font-medium">AI Startup Landing Page</div>
+        </div>
+        <nav className="flex flex-col lg:flex-row gap-5 lg:gap-7 lg:justify-end lg:flex-1">
+          <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition" >Home</a>
+          <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Features</a>
+          <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Pricing</a>
+          <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">Contact</a>
+          <a href="#" className="text-white/70 hover:text-white text-xs md:text-sm transition">asdasd</a>
+        </nav>
+        <div className="flex gap-5 lg:flex-1 lg:justify-end">
+          <XSocial className="text-white/40 hover:text-white transition" />
+          <InstaSocial className="text-white/40 hover:text-white transition" />
+          <YTSocial className="text-white/40 hover:text-white transition" />
+        </div>
+      </div>
+    </div>
+  </footer>;
 };
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Acme } from "next/font/google";
 import acmeLogo from "@/assets/logo-acme.png";
 import apexLogo from "@/assets/logo-apex.png";
@@ -5,7 +6,7 @@ import celestiallogo from '@/assets/logo-celestial.png';
 import quantumLogo from '@/assets/logo-quantum.png';
 import pulseLogo from '@/assets/logo-pulse.png';
 import echoLogo from '@/assets/logo-echo.png';
-
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return <section className="py-20 md:py-24">
@@ -14,12 +15,20 @@ export const LogoTicker = () => {
         <div className="flex-1 md:flex-none">
           <h2>Trusted by Top-Companies: </h2>
         </div>
-        <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-          <div className="flex flex-none gap-14">
-            {[acmeLogo, pulseLogo, celestiallogo, apexLogo, quantumLogo, echoLogo].map((logo, index) => (
+        <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <motion.div initial={{
+            translateX: '-50%',
+          }} animate={{
+            translateX: '0',
+          }} transition={{
+            repeat: Infinity,
+            duration: 20,
+            ease: 'linear',
+          }} className="flex flex-none gap-14 pr-14 -translate-x-1/2">
+            {[acmeLogo, pulseLogo, celestiallogo, apexLogo, quantumLogo, echoLogo,acmeLogo, pulseLogo, celestiallogo, apexLogo, quantumLogo, echoLogo].map((logo, index) => (
               <img key={index} src={logo.src} alt="logo" className="h-6 w-auto" />
             ))}
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
