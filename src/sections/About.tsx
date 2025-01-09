858bbad95f4de733c7ed1a4b64563fceb85ce7d7
Himanshu Seervi <himseervi@gmail.com>
2025-01-08 21:00:32 -0500

    Changed the about header

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
                 <div className="max-w-6xl w-full p-8 rounded-lg shadow-lg">
                     <div className="flex flex-col md:flex-row items-center">
                         <div className="md:w-1/2 md:pr-8 mb-8 md:mb-0">
-                            <h1 className="text-3xl md:text-4xl font-medium tracking-tighter text-left mb-5">About Us</h1>
+                            <h1 className="text-3xl md:text-4xl font-medium tracking-tighter text-left mb-5">About Me:</h1>
                             <p className="text-white/70">
                                 Hi, I&apos;m Himanshu Seervi, I&apos;m a second-year Software Engineering student at the University of Guelph. With almost a year of experience in web design and development at <a href="https://guelphentrepreneurshipsociety.com/" className='underline decoration-blue-500 hover:underline hover:decoration-blue-800 duration-150'> Guelph Entrepreneurship Society</a>, and an additional four months focused on developing <a href="https://www.aveva.com/en/products/aveva-pi-vision/" className='underline decoration-blue-500 hover:underline hover:decoration-blue-800 duration-150'>PI Vision</a> Data Interface Web Application at <a href="https://algoma.com/" className='underline decoration-blue-500 hover:underline hover:decoration-blue-800 duration-150'>Algoma Steel</a>, I&apos;ve honed my skills in crafting innovative and efficient digital solutions.
                             </p>
